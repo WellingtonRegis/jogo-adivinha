c497fab2826b34dc8cc90fd6fd2eb1357b252095
WellingtonRegis <ricardowellington854@gmail.com>
2023-06-12 08:48:17 -0300

    Correcao de logica de errar letra

--- jogoAdivinha.py
+++ jogoAdivinha.py
@@ -23,7 +23,8 @@ def atualizar_tela():
     tentativas_label.config(text=f'Você tem {tentativas} tentativas')
     letras_digitadas_label.config(text=f'Letras digitadas: {letras_digitadas}')
 
-def verificar_letra(tentativas=5):
+def verificar_letra():
+    global tentativas
     letra = letra_entry.get().lower()
     letras_digitadas.append(letra)
     encontrar_letra = False
@@ -40,11 +41,11 @@ def verificar_letra(tentativas=5):
         tentativas -= 1
 
     if ' - ' not in letras_adivinhadas:
-        resultado_label.config(text=f'Parabéns! Você acertou a palavra secreta: {palavra_escolhida}')
+        resultado_label.config(text=f'Parabéns! Você acertou a palavra secreta: {palavra_escolhida}'.upper())
         letra_entry.config(state=tk.DISABLED, width=30, justify=tk.CENTER)
         verificar_button.config(state=tk.DISABLED)
     elif tentativas == 0:
-        resultado_label.config(text=f'Desculpe! Infelizmente você excedeu o número de tentativas e perdeu o jogo. A palavra secreta era: {palavra_escolhida}')
+        resultado_label.config(text=f'Desculpe! Infelizmente você excedeu o número de tentativas e perdeu o jogo. A palavra secreta era: {palavra_escolhida}'.upper())
         letra_entry.config(state=tk.DISABLED, width=30, justify=tk.CENTER)
         verificar_button.config(state=tk.DISABLED)
 
